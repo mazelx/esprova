efbf07f9702a41885709f069e5332703821213ae
mazelx <mazelx@gmail.com>
2014-12-29 15:29:18 +0100

    add haystack connection

--- qoors/settings.py
+++ qoors/settings.py
@@ -109,6 +109,13 @@ STATICFILES_DIRS = (
 )
 
 
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://127.0.0.1:8983/solr'
+    },
+}
+
  # ------ Project Specific Settings ------
 
 GOOGLE_API_KEY = "AIzaSyCA3YCeUu02CRg_QPLS8GUhIx2fgX4is24"
