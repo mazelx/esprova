67667ca26d7f60050d0c252d8477d51515d2ec21
mazelx <mazelx@gmail.com>
2014-12-30 19:28:59 +0100

    branch to be deleted

--- core/search_indexes.py
+++ core/search_indexes.py
@@ -1,10 +1,13 @@
-class JobIndex(indexes.SearchIndex, indexes.Indexable):
+from core.models import Event, Race
+from haystack import indexes
+
+
+class EventIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    type = indexes.CharField(model_attr='type', faceted=True)
-    location = indexes.CharField(model_attr='location', faceted=True)
+    name = indexes.CharField(model_attr='name', faceted=True)
 
     def get_model(self):
-        return Job
+        return Event
 
     def index_queryset(self):
         return self.get_model().objects.all()
