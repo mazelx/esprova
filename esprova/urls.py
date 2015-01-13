3d7108470c55c1981baeb82dfc28239711f1440b
mazelx <mazelx@gmail.com>
2015-01-13 21:48:04 +0100

    robots.txt rule to forbid search bots

--- esprova/urls.py
+++ esprova/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from core.views import RaceView, RaceList, getRacesAjax, RaceWizard
 from core.forms import ContactForm, RaceForm, LocationForm, EventForm
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 race_named_forms = (
     ("event", EventForm),
@@ -15,6 +16,7 @@ create_race = RaceWizard.as_view(race_named_forms, url_name="create_race_step")
 
 
 urlpatterns = patterns('',
+                       url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),                   
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', RaceList.as_view(), name='list_race'),
                        url(r'^race/(?P<pk>\d+)$', RaceView.as_view(), name='view_race'),
@@ -25,3 +27,5 @@ urlpatterns = patterns('',
 
 # serve static files on dev
 urlpatterns += staticfiles_urlpatterns()
+
+
