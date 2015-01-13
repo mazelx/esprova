380435addf3802884b0da6076001924c61842374
mazelx <mazelx@gmail.com>
2015-01-13 17:00:22 +0100

    remove static local folders

--- esprova/settings.py
+++ esprova/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/www/esprova/static/',
-)
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+#     '/www/esprova/static/',
+# )
 
  # ------ Project Specific Settings ------
 
