6bad1830b4c75dac2a60bbe9dd8aa46d695e4a41
mazelx <mazelx@gmail.com>
2015-01-15 17:39:57 +0100

    fix staticfiles settings for storing on s3

--- esprova/settings.py
+++ esprova/settings.py
@@ -141,8 +141,7 @@ NOSE_ARGS = [
 # Where to find the static files ?
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static_out")
 
 # AWS -----------
 
@@ -165,7 +164,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
