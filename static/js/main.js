b0d0756d093ee1a735dfa8899071d8484ac49df5
mazelx <mazelx@gmail.com>
2015-01-15 11:30:38 +0100

    store map style in a separate file and switch map type default to terrain

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,11 @@ var markers = {};
 var selected_event_id;
 var highestZIndex = 10;
 
+// if the map style has not been provided
+if (typeof map_styles === 'undefined'){
+    map_styles = [];
+}
+
 var defaultMarkerIcon = {
         url: 'https://esprova-static.s3.amazonaws.com/images/marker_icon.svg',
         size: new google.maps.Size(28,42),
@@ -18,243 +23,6 @@ var hoveredMarkerIcon = {
         size: new google.maps.Size(28,42),
 };
 
-var styles =[
-{
-    "featureType": "administrative",
-    "elementType": "labels.text.fill",
-    "stylers": [
-        {
-            "color": "#b1b1b1"
-        }
-    ]
-},
-{
-    "featureType": "administrative",
-    "elementType": "labels.icon",
-    "stylers": [
-        {
-            "visibility": "simplified"
-        },
-        {
-            "color": "#d3d3d3"
-        }
-    ]
-},
-{
-    "featureType": "administrative.country",
-    "elementType": "geometry.fill",
-    "stylers": [
-        {
-            "color": "#a9b4c1"
-        },
-        {
-            "visibility": "off"
-        }
-    ]
-},
-{
-    "featureType": "administrative.country",
-    "elementType": "geometry.stroke",
-    "stylers": [
-        {
-            "visibility": "on"
-        },
-        {
-            "color": "#94a1b8"
-        }
-    ]
-},
-{
-    "featureType": "administrative.country",
-    "elementType": "labels.text.fill",
-    "stylers": [
-        {
-            "color": "#708991"
-        },
-        {
-            "visibility": "on"
-        },
-        {
-            "weight": "1.00"
-        }
-    ]
-},
-{
-    "featureType": "administrative.country",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-        {
-            "visibility": "off"
-        }
-    ]
-},
-{
-    "featureType": "administrative.country",
-    "elementType": "labels.icon",
-    "stylers": [
-        {
-            "visibility": "off"
-        }
-    ]
-},
-{
-    "featureType": "administrative.locality",
-    "elementType": "all",
-    "stylers": [
-        {
-            "weight": "4.68"
-        }
-    ]
-},
-{
-    "featureType": "landscape",
-    "elementType": "all",
-    "stylers": [
-        {
-            "color": "#f2f2f2"
-        }
-    ]
-},
-{
-    "featureType": "landscape",
-    "elementType": "geometry.fill",
-    "stylers": [
-        {
-            "color": "#ffffff"
-        }
-    ]
-},
-{
-    "featureType": "landscape.natural.terrain",
-    "elementType": "geometry.fill",
-    "stylers": [
-        {
-            "visibility": "on"
-        },
-        {
-            "color": "#b8c5bb"
-        }
-    ]
-},
-{
-    "featureType": "poi",
-    "elementType": "all",
-    "stylers": [
-        {
-            "visibility": "off"
-        }
-    ]
-},
-{
-    "featureType": "road",
-    "elementType": "all",
-    "stylers": [
-        {
-            "saturation": -100
-        },
-        {
-            "lightness": 45
-        }
-    ]
-},
-{
-    "featureType": "road",
-    "elementType": "geometry.fill",
-    "stylers": [
-        {
-            "color": "#b1b1b1"
-        }
-    ]
-},
-{
-    "featureType": "road.highway",
-    "elementType": "all",
-    "stylers": [
-        {
-            "visibility": "simplified"
-        }
-    ]
-},
-{
-    "featureType": "road.arterial",
-    "elementType": "geometry.fill",
-    "stylers": [
-        {
-            "visibility": "on"
-        }
-    ]
-},
-{
-    "featureType": "road.arterial",
-    "elementType": "labels.icon",
-    "stylers": [
-        {
-            "visibility": "off"
-        }
-    ]
-},
-{
-    "featureType": "road.local",
-    "elementType": "geometry.fill",
-    "stylers": [
-        {
-            "color": "#ff0000"
-        },
-        {
-            "visibility": "on"
-        }
-    ]
-},
-{
-    "featureType": "transit",
-    "elementType": "all",
-    "stylers": [
-        {
-            "visibility": "off"
-        }
-    ]
-},
-{
-    "featureType": "water",
-    "elementType": "all",
-    "stylers": [
-        {
-            "color": "#30f557"
-        },
-        {
-            "visibility": "on"
-        }
-    ]
-},
-{
-    "featureType": "water",
-    "elementType": "geometry.fill",
-    "stylers": [
-        {
-            "color": "#7ae8f9"
-        }
-    ]
-},
-{
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-        {
-            "color": "#65868d"
-        }
-    ]
-},
-{
-    "featureType": "water",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-        {
-            "visibility": "off"
-        }
-    ]
-}
-];
-
 
 // ----------------------
 // Document initialization
@@ -264,6 +32,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 function initialize() {
     var mapOptions = {
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
         center: {
             lat: 46.9,
             lng: 2.6
@@ -279,7 +48,7 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
-    map.setOptions({styles: styles});
+    map.setOptions({styles: map_styles});
 
     // On Firefox (others?), a refresh would not display markers
     google.maps.event.addListenerOnce(map, 'idle', function() {
