57f532bf5f0117b1c07fbf8424d63b0704a50b4b
mazelx <mazelx@gmail.com>
2015-01-27 00:15:07 +0100

    addd humanize model

--- esprova/settings.py
+++ esprova/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.formtools',
+    'django.contrib.humanize',
     # External modules
     'rest_framework',
     'django_countries',
