4ca1d21c1b7757506c9b73d9007923d819bb6b66
mazelx <mazelx@gmail.com>
2015-03-03 19:54:40 +0100

    fix locale for mac os AND linux (utf8 / utf-8)

--- core/templatetags/templatefilters.py
+++ core/templatetags/templatefilters.py
@@ -1,10 +1,14 @@
 from django import template
-from datetime import date, timedelta
-import locale, datetime
+import locale
 
 register = template.Library()
 
+
 @register.filter(name='datefr')
 def datefr(value):
-    locale.setlocale(locale.LC_TIME, "fr_FR")
+    try:
+        locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
+    except Exception:
+        locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+
     return value.strftime("%A %d/%m/%Y à %H:%M")
