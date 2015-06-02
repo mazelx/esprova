932ff63422d9e19ab492ef6010d4b883ed9bc3af
mazelx <mazelx@gmail.com>
2015-06-02 12:52:41 +0200

    add map on location form

--- static/js/create.js
+++ static/js/create.js
@@ -1,19 +1,23 @@
  
 var native_datepicker = Modernizr.touch;
 
+var mapOptions;
+var marker;
+
+
 
 var componentForm = {
   street_number : [{id: "id_location-street_number", selector: "short_name"}],
   route : [{id: "id_location-route", selector: "short_name"}],
   locality : [{id: "id_location-locality", selector: "short_name"}],
-  administrative_area_level_1 : [
-    {id: "id_location-administrative_area_level_1_short_name", selector: "short_name"},
-    {id: "id_location-administrative_area_level_1", selector: "long_name"}
-    ],
-  administrative_area_level_2 : [
-    {id: "id_location-administrative_area_level_2_short_name", selector: "short_name"},
-    {id: "id_location-administrative_area_level_2", selector: "long_name"}
-    ],
+  // administrative_area_level_1 : [
+  //   {id: "id_location-administrative_area_level_1_short_name", selector: "short_name"},
+  //   {id: "id_location-administrative_area_level_1", selector: "long_name"}
+  //   ],
+  // administrative_area_level_2 : [
+  //   {id: "id_location-administrative_area_level_2_short_name", selector: "short_name"},
+  //   {id: "id_location-administrative_area_level_2", selector: "long_name"}
+  //   ],
   country : [{id: "id_location-country", selector: "short_name"}],
   postal_code : [{id: "id_location-postal_code", selector: "short_name"}],
 };
@@ -23,16 +27,47 @@ $(document).ready(function() {
 });
 
 function initialize() {
-  // Create the autocomplete object, restricting the search
-  // to geographical location types.
+  locForm = false;
   if (typeof google === 'object' && typeof google.maps === 'object'){
+
+  if(typeof($("#id_location-lat")) !== "undefined"){
+    lat = parseFloat( $("#id_location-lat").val());
+    lng = parseFloat( $("#id_location-lng").val());
+    locForm = true;
+  }
+    
+    mapOptions = {
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        center: {
+            lat: lat,
+            lng: lng
+        },
+        zoom: 10,
+        maxZoom: 15,
+        // minZoom:5,   
+        panControl: false,
+        zoomControl: true,
+        streetViewControl: false
+    };
+
+    if(locForm) {
+      createMap();
+    }
+    // create map
+
+
+
+    // Create the autocomplete object, restricting the search
+    // to geographical location types.
     autocomplete = new google.maps.places.Autocomplete(
         (document.getElementById('autocompleteInput')),{ types: ['geocode'] });
     // When the user selects an address from the dropdown,
     // populate the address fields in the form.
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      fillInAddress();
+      clearForm();
+      changeAddress();
     });
+
   }
 
   if (!native_datepicker) {
@@ -53,8 +88,58 @@ function createDatePickerComponent() {
     });
 }
 
+function createMap(){
+    if (typeof map_styles === 'undefined'){
+        map_styles = [];
+    }
+        map = new google.maps.Map(document.getElementById('address-map'),
+            mapOptions);
+        map.setOptions({styles: map_styles});
+
+        marker = new google.maps.Marker({
+            position: {lat:lat, lng:lng} ,
+            map: map,
+            zIndex : 1,
+        });
+}   
+
+function clearForm() {
+  var frm_elements = document.getElementById("edit-form").elements
+  for(i=0; i<frm_elements.length; i++) {
+    field_type = frm_elements[i].type.toLowerCase();
+    
+    // do not clear the crsf token
+    if(frm_elements[i].name === 'csrfmiddlewaretoken'){
+      break;
+    }
+
+    switch (field_type)
+    {
+    case "text":
+    case "password":
+    case "textarea":
+    case "hidden":
+        frm_elements[i].value = "";
+        break;
+    case "radio":
+    case "checkbox":
+        if (frm_elements[i].checked)
+        {
+            frm_elements[i].checked = false;
+        }
+        break;
+    case "select-one":
+    case "select-multi":
+        frm_elements[i].selectedIndex = -1;
+        break;
+    default:
+        break;
+    }
+  }
+}
+
 // [START region_fillform]
-function fillInAddress() {
+function changeAddress() {
   // Get the place details from the autocomplete object.
   var place = autocomplete.getPlace();
 
@@ -67,10 +152,10 @@ function fillInAddress() {
 
     var addressType = place.address_components[i].types[0];
 
-    // find form components correpsonding to that address type
+    // find form components corresponding to that address type
     var componentArray = componentForm[addressType]
  
-    if (componentArray.length) { 
+    if (typeof(componentArray) !== 'undefined') { 
       for (var j = 0; j < componentArray.length; j++) {
         var val = place.address_components[i][componentArray[j].selector];
         document.getElementById(componentArray[j].id).value = val;
@@ -78,9 +163,24 @@ function fillInAddress() {
     }
 
   }
+  // remove the marker from the map
+  marker.setMap(null);
 
-    document.getElementById('id_location-lat').value = place.geometry.location.lat().toFixed(5);
-    document.getElementById('id_location-lng').value = place.geometry.location.lng().toFixed(5);
+  lat = place.geometry.location.lat();
+  lng = place.geometry.location.lng();
+
+  marker = new google.maps.Marker({
+          position: {lat:lat, lng:lng} ,
+          map: map,
+          zIndex : 1,
+      });
+
+  // center map on new marker
+  map.setCenter(marker.getPosition());
+
+  // save the lat /lng in the form
+  document.getElementById('id_location-lat').value = lat.toFixed(5);
+  document.getElementById('id_location-lng').value = lng.toFixed(5);
 }
 // [END region_fillform]
 
