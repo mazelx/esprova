c7e46ef2032991dace574a181fc96dc732e26ed5
mazelx <mazelx@gmail.com>
2015-06-07 15:33:39 +0200

    remove event haystack signal (not working)

--- core/signals.py
+++ core/signals.py
@@ -1,20 +1,16 @@
-from core.models import Race, Event
+from core.models import Race
 from django.db import models
 from haystack import signals
 
 
 class RaceOnlySignalProcessor(signals.BaseSignalProcessor):
     def setup(self):
-        # Listen only to the ``Race`` and ''event'' model.
+        # Listen only to the ``Race`` model.
         models.signals.post_save.connect(self.handle_save, sender=Race)
         models.signals.post_delete.connect(self.handle_delete, sender=Race)
-        models.signals.post_save.connect(self.handle_save, sender=Event)
-        models.signals.post_delete.connect(self.handle_delete, sender=Event)
 
     def teardown(self):
         # Disconnect only for the ``Race`` model.
         models.signals.post_save.disconnect(self.handle_save, sender=Race)
         models.signals.post_delete.disconnect(self.handle_delete, sender=Race)
-        models.signals.post_save.disconnect(self.handle_save, sender=Event)
-        models.signals.post_delete.disconnect(self.handle_delete, sender=Event)
-        
+
