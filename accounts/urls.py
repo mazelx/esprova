124cd1659c5afa216316dca6b9ab5171346a4cd9
mazelx <mazelx@gmail.com>
2015-06-26 00:12:50 +0200

    fix url for registration (default view was used)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url, patterns, include
 from accounts.views import *
 
 urlpatterns = patterns('',
-                       url(r'^register$', CustomRegistrationView.as_view(), name="registration_register"),
-                       url(r'^reset/done$', custom_password_reset_done, name="auth_password_reset_done"),
+                       url(r'^register/$', CustomRegistrationView.as_view(), name="registration_register"),
+                       url(r'^reset/done/$', custom_password_reset_done, name="auth_password_reset_done"),
                        url(r'^password/change/done/$', custom_password_change_done, name="auth_password_change_done"),
-                       url(r'^reset/done$', custom_password_reset_done, name="auth_password_reset_done"),
-                       url(r'^settings$', UserSettingsView.as_view(), name="user_settings"),
+                       url(r'^reset/done/$', custom_password_reset_done, name="auth_password_reset_done"),
+                       url(r'^settings/$', UserSettingsView.as_view(), name="user_settings"),
                        url(r'', include('registration.backends.default.urls')),
                        )
