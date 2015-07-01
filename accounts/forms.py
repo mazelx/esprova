ab31249d12b885aa88dfe9bbb0c64dfbfd13e6c3
mazelx <mazelx@gmail.com>
2015-07-01 15:15:26 +0200

    force uniquess of username (case insensitive

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,21 @@
-# from registration.forms import RegistrationFormUniqueEmail
-# from passwords.fields import PasswordField
+from django import forms
+from registration.forms import RegistrationFormUniqueEmail
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
-# class customRegistrationForm(RegistrationFormUniqueEmail):
-#     password = PasswordField(label="Password")
+class customRegistrationForm(RegistrationFormUniqueEmail):
+    """
+    Subclass of ``RegistrationForm`` which enforces uniqueness of
+    username (case insensitive)
+
+    """
+    def clean_username(self):
+        """
+        Validate that the supplied username is unique for the
+        site.
+
+        """
+        if User.objects.filter(username__iexact=self.cleaned_data['username']):
+            raise forms.ValidationError(_("This username is already in use. Please supply a different username."))
+        return self.cleaned_data['username']
