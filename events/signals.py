c1174c2fb6ad398f93a2827287b838a4c7153b11
mazelx <mazelx@gmail.com>
2015-08-06 11:23:09 +0200

    wrap event and race dependancy deletion in try/except

--- events/signals.py
+++ events/signals.py
@@ -1,20 +1,29 @@
 from events.models import Race, Event
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @receiver(post_delete, sender=Race)
 def post_delete_race(sender, instance, *args, **kwargs):
-    if instance.contact:
+    try:
         if instance.contact.pk:
             instance.contact.delete()
-    if instance.location:
+    except ObjectDoesNotExist:
+        pass
+
+    try:
         if instance.location.pk:
             instance.location.delete()
+    except ObjectDoesNotExist:
+        pass
 
 
 @receiver(post_delete, sender=Event)
 def post_delete_event(sender, instance, *args, **kwargs):
-    if instance.organizer:
-        if instance.organizer.pk and instance.organizer.event_set.count() == 0:
-            instance.organizer.delete()
+    try:
+        if instance.organizer.pk:
+            if instance.organizer.event_set.count() == 0:
+                instance.organizer.delete()
+    except ObjectDoesNotExist:
+        pass
