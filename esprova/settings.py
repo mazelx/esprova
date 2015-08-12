c1a23e41142210bf6f5a9fbd9bb567e6fbe5849f
mazelx <mazelx@gmail.com>
2015-08-12 14:51:43 +0200

    typo

--- esprova/settings.py
+++ esprova/settings.py
@@ -163,13 +163,13 @@ GOOGLE_API_KEY = "AIzaSyAu5lWzzuB7WXLqI9UzK2yL0IVtyr97yOg"
 
 GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID') or 'UA-60175871-1'
 
-elasticsearch_url = os.environ.get('SEARCHBOX_SSL_URL')
+ELASTICSEARCH_URL = os.environ.get('SEARCHBOX_SSL_URL')
 
 # Haystack -----------
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'search_backends.elastic_backend.CustomElasticSearchEngine',
-        'URL': elasticsearch_url,
+        'URL': ELASTICSEARCH_URL,
         'INDEX_NAME': 'haystack',
     },
 }
